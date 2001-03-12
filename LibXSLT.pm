7ff763aad380cfc458f700e4fcf43955be0633c8
matt <matt@48c0daa5-cf15-4c16-847c-abda57616a86>
2001-03-12 20:19:47 +0000

    Added docs.

--- LibXSLT.pm
+++ LibXSLT.pm
@@ -9,7 +9,7 @@ use XML::LibXML;
 
 require Exporter;
 
-$VERSION = "0.92";
+$VERSION = "0.94";
 
 require DynaLoader;
 
@@ -26,31 +26,97 @@ sub new {
 
 1;
 __END__
-# Below is the stub of documentation for your module. You better edit it!
 
 =head1 NAME
 
-XML::LibXSLT - Perl extension for blah blah blah
+XML::LibXSLT - Interface to the gnome libxslt library
 
 =head1 SYNOPSIS
 
   use XML::LibXSLT;
-  blah blah blah
+  use XML::LibXML;
+  
+  my $parser = XML::LibXML->new();
+  my $xslt = XML::LibXSLT->new();
+  
+  my $source = $parser->parse_file('foo.xml');
+  my $style_doc = $parser->parse_file('bar.xsl');
+  
+  my $stylesheet = $xslt->parse_stylesheet($style_doc);
+  
+  my $results = $stylesheet->transform($source);
+  
+  print $stylesheet->output_string($results);
 
 =head1 DESCRIPTION
 
-Stub documentation for XML::LibXSLT was created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+This module is an interface to the gnome project's libxslt. This is an
+extremely good XSLT engine, highly compliant and also very fast. I have
+tests showing this to be more than twice as fast as Sablotron.
 
-Blah blah blah.
+=head1 OPTIONS
+
+When calling C<new()>, the following options are available (passed in
+as a hash of key/value pairs):
+
+=over
+
+=item error_handler
+
+A subroutine reference or function name that is called whenever an XSLT
+processsing error occurs. Note that this is not the same as an XML
+parsing error - for that see L<XML::LibXSLT>.
+
+=back
+
+=head1 API
+
+The following methods are available on the new XML::LibXSLT object:
+
+=head2 parse_stylesheet($doc)
+
+C<$doc> here is an XML::LibXML::Document object (see L<XML::LibXML>)
+representing an XSLT file. This method will return a 
+XML::LibXSLT::Stylesheet object, or undef on failure.
+
+=head1 XML::LibXSLT::Stylesheet
+
+The main API is on the stylesheet, though it is fairly minimal.
+
+One of the main advantages of XML::LibXSLT is that you have a generic
+stylesheet object which you call the transform() method passing in a
+document to transform. This allows you to have multiple transformations
+happen with one stylesheet without requiring a reparse.
+
+=head2 transform($doc)
+
+Transforms the passed in XML::LibXML::Document object, and returns a
+new XML::LibXML::Document.
+
+=head2 output_string($result)
+
+Returns a scalar that is the XSLT rendering of the XML::LibXML::Document
+object using the desired output format (specified in the xsl:output tag
+in the stylesheet). Note that you can also call $result->toString, but
+that will *always* output the document in XML format, and in UTF8, which
+may not be what you asked for in the xsl:output tag.
+
+=head2 output_fh($result, $fh)
+
+Outputs the result to the filehandle given in C<$fh>.
+
+=head2 output_file($result, $filename)
+
+Outputs the result to the file named in C<$filename>.
 
 =head1 AUTHOR
 
-A. U. Thor, a.u.thor@a.galaxy.far.far.away
+Matt Sergeant, matt@sergeant.org
+
+Copyright 2001, AxKit.com Ltd. All rights reserved.
 
 =head1 SEE ALSO
 
-perl(1).
+XML::LibXML
 
 =cut
