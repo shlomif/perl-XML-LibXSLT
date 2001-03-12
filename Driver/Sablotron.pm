6e8e1be50a4df7efed834207f1ac3398301007e5
matt <matt@48c0daa5-cf15-4c16-847c-abda57616a86>
2001-03-12 15:27:19 +0000

    Add error handler stuff

--- Driver/Sablotron.pm
+++ Driver/Sablotron.pm
@@ -11,10 +11,23 @@ use vars qw(
         $xslt
         $stylesheet
         $input
+        $handler
         );
 
 sub init {
+    my %options = @_;
     $xslt = XML::Sablotron->new();
+    $handler = Driver::Sablotron::Handler->new(%options);
+    $xslt->RegHandler(0, $handler);
+}
+
+sub shutdown {
+    $xslt->ClearError();
+    $xslt->UnregHandler(0, $handler);
+    undef $handler;
+    undef $xslt;
+    undef $input;
+    undef $stylesheet;
 }
 
 sub load_stylesheet {
@@ -46,13 +59,44 @@ sub run_transform {
         my $result = '';
         my $args = ['template', "$stylesheet", 'xml_resource', "$input"];
 
-        my $retcode = $xslt->RunProcessor("arg:/template", "arg:/xml_resource", "arg:/result",
-                                                [], $args);
+        my $retcode = $xslt->RunProcessor("arg:/template", "arg:/xml_resource", "arg:/result",                                                [], $args);
         $result = $xslt->GetResultArg("result");
-
+        
         print $outfile $result;
         $outfile->close;
     }
+}
+
+package Driver::Sablotron::Handler;
+
+sub new {
+    my $class = shift;
+    my %options = @_;
+    bless \%options, $class;
+}
+
+sub MHMakeCode {
+    my $self = shift;
+    my $processor = shift;
+
+    my ($severity, $facility, $code) = @_;
+    return $code;
+}
+
+sub MHLog {
+    my $self = shift;
+    my $processor = shift;
+    
+    warn "Sablotron [Log]: ", join(' :: ', @fields), "\n" if $self->{verbose};
+    return 1;
+}
+
+sub MHError {
+    my $self = shift;
+    my $processor = shift;
+    
+    warn "Sablotron [Error]: ", join(' :: ', @fields), "\n" if $self->{verbose};
+    return 1;
 }
 
 1;
